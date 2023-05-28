3d800443dffd6e7e026b98e67a1d72082712e283
brajpatel <brajeshpatel0002@gmail.com>
2023-05-28 20:44:20 +0100

    Add hover effect to sidebar elements

--- src/components/signed-out-package/sidebar/SignedOutSidebarSmall.js
+++ src/components/signed-out-package/sidebar/SignedOutSidebarSmall.js
@@ -15,32 +15,32 @@ function SignedOutSidebarSmall(props) {
                 <RxHamburgerMenu/>
             </div>
             <ul>
-                <Link to="/">
-                    <li className="page-link">
+                <Link to="/" className="page-link">
+                    <li>
                         <MdHomeFilled className="sidebar-icon"/>
                         <p>Home</p>
                     </li>
                 </Link>
-                <Link to="/">
-                    <li className="page-link">
+                <Link to="/" className="page-link">
+                    <li>
                         <MdOutlineExplore className="sidebar-icon"/>
                         <p>Explore</p>
                     </li>
                 </Link>
-                <Link to="/">
-                    <li className="page-link">
+                <Link to="/" className="page-link">
+                    <li>
                         <BsCollectionPlay className="sidebar-icon"/>
                         <p>Subscriptions</p>
                     </li>
                 </Link>
-                <Link to="/">
-                    <li className="page-link">
+                <Link to="/" className="page-link">
+                    <li>
                         <MdOutlineVideoLibrary className="sidebar-icon"/>
                         <p>Library</p>
                     </li>
                 </Link>
-                <Link to="/">
-                    <li className="page-link">
+                <Link to="/" className="page-link">
+                    <li>
                         <GrHistory className="sidebar-icon"/>
                         <p>History</p>
                     </li>
