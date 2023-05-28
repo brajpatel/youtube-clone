32ea932097b48fffaf4e1c08f10bf35e6d441632
brajpatel <brajeshpatel0002@gmail.com>
2023-05-28 11:48:40 +0100

    Structure sidebar elements

--- src/components/signed-out-package/sidebar/SignedOutSidebarSmall.js
+++ src/components/signed-out-package/sidebar/SignedOutSidebarSmall.js
@@ -5,10 +5,36 @@ function SignedOutSidebarSmall() {
         <nav>
             <div>Hamburger</div>
             <ul>
-                <li>
-                    <i class="fa-solid fa-house"></i>
-                    <p>Home</p>
-                </li>
+                <Link to="/">
+                    <li className="page-link">
+                        <p>Svg here</p>
+                        <p>Home</p>
+                    </li>
+                </Link>
+                <Link to="/">
+                    <li className="page-link">
+                        <p>Svg here</p>
+                        <p>Shorts</p>
+                    </li>
+                </Link>
+                <Link to="/">
+                    <li className="page-link">
+                        <p>Svg here</p>
+                        <p>Subscriptions</p>
+                    </li>
+                </Link>
+                <Link to="/">
+                    <li className="page-link">
+                        <p>Svg here</p>
+                        <p>Library</p>
+                    </li>
+                </Link>
+                <Link to="/">
+                    <li className="page-link">
+                        <p>Svg here</p>
+                        <p>History</p>
+                    </li>
+                </Link>
             </ul>
         </nav>
     )
