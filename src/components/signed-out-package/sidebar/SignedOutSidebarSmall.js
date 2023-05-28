d5c392968ef60345d987e89f1a5dc8279a4a1bc3
brajpatel <brajeshpatel0002@gmail.com>
2023-05-28 11:35:42 +0100

    Add youtube favicon

--- src/components/signed-out-package/sidebar/SignedOutSidebarSmall.js
+++ src/components/signed-out-package/sidebar/SignedOutSidebarSmall.js
@@ -3,7 +3,13 @@ import { Link } from "react-router-dom";
 function SignedOutSidebarSmall() {
     return (
         <nav>
-            
+            <div>Hamburger</div>
+            <ul>
+                <li>
+                    <i class="fa-solid fa-house"></i>
+                    <p>Home</p>
+                </li>
+            </ul>
         </nav>
     )
 }
