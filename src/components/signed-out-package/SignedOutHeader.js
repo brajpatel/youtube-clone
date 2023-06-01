2c101345e9b524bbc09c0e47f2ae20415e56ed00
brajpatel <brajeshpatel0002@gmail.com>
2023-06-01 10:03:40 +0100

    Add button to clear search bar

--- src/components/signed-out-package/SignedOutHeader.js
+++ src/components/signed-out-package/SignedOutHeader.js
@@ -1,15 +1,28 @@
 import "./SignedOutHeader.css";
 import youtubeLogo from "../../assets/youtube-logo.png";
 import { RxMagnifyingGlass } from "react-icons/rx";
+import { RxCross1 } from "react-icons/rx";
 import SignInButton from "./SignInButton";
+import { useState } from "react";
 
 function SignedOutHeader() {
+    const [searchInput, setSearchInput] = useState('');
+
+    const handleChange = (e) => {
+        setSearchInput(e.target.value);
+    }
+
+    const clearSearchBar = () => {
+        setSearchInput('');
+    }
+
     return (
         <header>
             <img className="youtube-logo" src={youtubeLogo} alt="youtube-logo"/>
             <div className="search-box">
-                <input type="text"/>
+                <input onChange={handleChange} placeholder="Search" value={searchInput} type="text"/>
                 <button><RxMagnifyingGlass/></button>
+                <p className={`clear-search-bar ${searchInput.length ? 'show' : ''}`} onClick={clearSearchBar}><RxCross1/></p>
             </div>
             <SignInButton/>
         </header>
