31ab044bb55381995fcd551a39d4fd58898f7e8b
brajpatel <brajeshpatel0002@gmail.com>
2023-06-04 10:31:44 +0100

    Add state for being signed in or not

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SignedOutPackage from './components/signed-out-package/SignedOutPackage';
 import SignedOutSubscriptions from './components/signed-out-package/signed-out-pages/SignedOutSubscriptions';
@@ -8,10 +9,12 @@ import SignedInPackage from './components/signed-in-package/SignedInPackage';
 import HomePage from './components/HomePage';
 
 function App() {
+  const [online, setOnline] = useState(true);
+
   return (
     <div className="app">
       <BrowserRouter>
-        <SignedOutPackage/>
+        {online ? <SignedInPackage/> : <SignedOutPackage/>}
         <Routes>
           <Route path="/" element={<HomePage/>}/>
           <Route path="/subscriptions" element={<SignedOutSubscriptions/>}/>
