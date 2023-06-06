12320548fa4d9eeb98421705466c66431fbfac5d
brajpatel <brajeshpatel0002@gmail.com>
2023-06-06 16:31:13 +0100

    Adjust search bar height

--- src/components/signed-in-package/SignedInHeader.js
+++ src/components/signed-in-package/SignedInHeader.js
@@ -1,5 +1,4 @@
 import "./SignedInHeader.css";
-import { useState } from "react";
 import youtubeLogo from "../../assets/youtube-logo.png";
 import SearchBar from "../SearchBar";
 import { FaMicrophone } from "react-icons/fa";
@@ -7,16 +6,6 @@ import { AiOutlinePlusSquare } from "react-icons/ai";
 import { BsBell } from "react-icons/bs";
 
 function SignedInHeader() {
-    const [searchInput, setSearchInput] = useState('');
-
-    const handleChange = (e) => {
-        setSearchInput(e.target.value);
-    }
-
-    const clearSearchBar = () => {
-        setSearchInput('');
-    }
-
     return (
         <header className="signed-in-header">
             <img className="youtube-logo" src={youtubeLogo} alt="youtube-logo"/>
