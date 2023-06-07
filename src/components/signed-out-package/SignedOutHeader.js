0693b7d78ff086671e9968edf23231e9b99928db
brajpatel <brajeshpatel0002@gmail.com>
2023-06-07 17:47:50 +0100

    Add those three little dots icon

--- src/components/signed-out-package/SignedOutHeader.js
+++ src/components/signed-out-package/SignedOutHeader.js
@@ -2,6 +2,7 @@ import "./SignedOutHeader.css";
 import { useState } from "react";
 import youtubeLogo from "../../assets/youtube-logo.png";
 import SearchBar from "../SearchBar";
+import { HiOutlineDotsVertical } from "react-icons/hi";
 import SignInButton from "./SignInButton";
 
 function SignedOutHeader() {
@@ -9,7 +10,10 @@ function SignedOutHeader() {
         <header className="signed-out-header">
             <img className="youtube-logo" src={youtubeLogo} alt="youtube-logo"/>
             <SearchBar/>
-            <SignInButton/>
+            <div className="header-end">
+                <HiOutlineDotsVertical className="header-icon"/>
+                <SignInButton/>
+            </div>
         </header>
     )
 }
