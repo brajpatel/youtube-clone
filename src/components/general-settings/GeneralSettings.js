970d5bb69a6349dc28df7422599efccfa7347c63
brajpatel <brajeshpatel0002@gmail.com>
2023-06-11 20:15:58 +0100

    Create general settings component

---
+++ src/components/general-settings/GeneralSettings.js
@@ -0,0 +1,44 @@
+import { HiOutlineMoon } from "react-icons/hi";
+import { LuLanguages } from "react-icons/lu";
+import { GrLanguage } from "react-icons/gr";
+import { FaRegKeyboard } from "react-icons/fa";
+import { BiCog } from "react-icons/bi";
+import { AiOutlineQuestionCircle } from "react-icons/ai";
+import { BiMessageError } from "react-icons/bi";
+
+function GeneralSettings() {
+    return (
+        <>
+            <li className='appearance'>
+                <HiOutlineMoon className='icon'/>
+                <p>Appearance: Light</p>
+            </li>
+            <li>
+                <LuLanguages className='icon'/>
+                <p>Language: British English</p>
+            </li>
+            <li>
+                <GrLanguage className='icon'/>
+                <p>Location: Earth</p>
+            </li>
+            <li>
+                <FaRegKeyboard className='icon'/>
+                <p>Keyboard shortcuts</p>
+            </li>
+            <li className='settings'>
+                <BiCog className='icon'/>
+                <p>Settings</p>
+            </li>
+            <li>
+                <AiOutlineQuestionCircle className='icon'/>
+                <p>Help</p>
+            </li>
+            <li>
+                <BiMessageError className='icon'/>
+                <p>Send feedback</p>
+            </li>
+        </>
+    )
+}
+
+export default GeneralSettings;
--- src/components/signed-in-package/account-settings/AccountSettings.js
+++ src/components/signed-in-package/account-settings/AccountSettings.js
@@ -1,13 +1,6 @@
 import './AccountSettings.css';
 import { FaRegUserCircle } from "react-icons/fa";
 import { MdExitToApp } from "react-icons/md";
-import { HiOutlineMoon } from "react-icons/hi";
-import { LuLanguages } from "react-icons/lu";
-import { GrLanguage } from "react-icons/gr";
-import { FaRegKeyboard } from "react-icons/fa";
-import { BiCog } from "react-icons/bi";
-import { AiOutlineQuestionCircle } from "react-icons/ai";
-import { BiMessageError } from "react-icons/bi";
 
 function AccountSettings() {
     return (
@@ -30,34 +23,7 @@ function AccountSettings() {
                     <MdExitToApp className='icon'/>
                     <p>Sign out</p>
                 </li>
-                <li className='appearance'>
-                    <HiOutlineMoon className='icon'/>
-                    <p>Appearance: Light</p>
-                </li>
-                <li>
-                    <LuLanguages className='icon'/>
-                    <p>Language: British English</p>
-                </li>
-                <li>
-                    <GrLanguage className='icon'/>
-                    <p>Location: Earth</p>
-                </li>
-                <li>
-                    <FaRegKeyboard className='icon'/>
-                    <p>Keyboard shortcuts</p>
-                </li>
-                <li className='settings'>
-                    <BiCog className='icon'/>
-                    <p>Settings</p>
-                </li>
-                <li>
-                    <AiOutlineQuestionCircle className='icon'/>
-                    <p>Help</p>
-                </li>
-                <li>
-                    <BiMessageError className='icon'/>
-                    <p>Send feedback</p>
-                </li>
+                
             </ul>
         </div>
     )
