6445d0f6b0384e2dadade912fccf9446a54b7c68
brajpatel <brajeshpatel0002@gmail.com>
2023-06-11 20:27:01 +0100

    Create general settings component

---
+++ src/components/signed-out-package/general-settings/GeneralSettings.js
@@ -0,0 +1,11 @@
+import ExtraSettings from "../../extra-settings/ExtraSettings";
+
+function GeneralSettings() {
+    return (
+        <div>
+            <ExtraSettings/>
+        </div>
+    )
+}
+
+export default GeneralSettings;
