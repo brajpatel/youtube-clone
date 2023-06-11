1475348e85874d324c37313a52a493ac5aa08517
brajpatel <brajeshpatel0002@gmail.com>
2023-06-11 20:29:21 +0100

    Render general settings

--- src/components/signed-out-package/header/SignedOutHeader.js
+++ src/components/signed-out-package/header/SignedOutHeader.js
@@ -1,11 +1,19 @@
 import "./SignedOutHeader.css";
+import { useState } from "react";
 import youtubeLogo from "../../../assets/youtube-logo.png";
 import SearchBar from "../../search-bar/SearchBar";
 import { HiOutlineDotsVertical } from "react-icons/hi";
+import GeneralSettings from "../general-settings/GeneralSettings";
 import SignInButton from "../sign-in-button/SignInButton";
 
 function SignedOutHeader(props) {
     const { setActiveTab } = props;
+
+    const [isVisible, setIsVisible] = useState(true);
+
+    const toggleAccountSettings = () => {
+        setIsVisible(!isVisible);
+    }
 
     return (
         <header className="signed-out-header">
@@ -13,7 +21,8 @@ function SignedOutHeader(props) {
             <SearchBar setActiveTab={setActiveTab}/>
             <div className="header-end">
                 <div className="header-icon settings">
-                    <HiOutlineDotsVertical/>
+                    <HiOutlineDotsVertical onClick={toggleAccountSettings}/>
+                    {isVisible ? <GeneralSettings/> : null}
                 </div>
                 <SignInButton/>
             </div>
