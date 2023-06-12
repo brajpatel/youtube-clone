3090404c8230fd37d39a8c0c74e55c8ab930b91b
brajpatel <brajeshpatel0002@gmail.com>
2023-06-12 17:56:30 +0100

    Create searched results page component

---
+++ src/components/searched-results-page/SearchedResultsPage.js
@@ -0,0 +1,11 @@
+import "./SearchedResultsPage.css";
+
+function SearchedResultsPage() {
+    return (
+        <div>
+            Searched Results Page
+        </div>
+    )
+}
+
+export default SearchedResultsPage;
