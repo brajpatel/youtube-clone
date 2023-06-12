fb8f07bafb61801210b0fc23db4fd276ca6d6049
brajpatel <brajeshpatel0002@gmail.com>
2023-06-12 17:51:59 +0100

    Import use navigate

--- src/components/search-bar/SearchBar.js
+++ src/components/search-bar/SearchBar.js
@@ -1,8 +1,8 @@
 import "./SearchBar.css";
 import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { RxMagnifyingGlass } from "react-icons/rx";
 import { RxCross1 } from "react-icons/rx";
-import { Link } from "react-router-dom";
 
 function SearchBar(props) {
     const { setActiveTab } = props;
