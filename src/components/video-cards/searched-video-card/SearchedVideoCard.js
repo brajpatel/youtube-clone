79e9ea210acaaed5b3cf53c20684f21f0eefbb41
brajpatel <brajeshpatel0002@gmail.com>
2023-06-13 11:00:22 +0100

    Style and position searched video card elements

--- src/components/video-cards/searched-video-card/SearchedVideoCard.js
+++ src/components/video-cards/searched-video-card/SearchedVideoCard.js
@@ -3,22 +3,24 @@ import { RxDotFilled } from "react-icons/rx";
 
 function SearchedVideoCard() {
     return (
-        <div>
-            <div>
-                <div>Image</div>
-                <p>11:29</p>
+        <div className="searched-video-card">
+            <div className="thumbnail">
+                <p>Image</p>
+                <p className="duration">11:29</p>
             </div>
 
-            <div>
-                <div>
+            <div className="info">
+                <div className="video-details">
                     <p className="video-title">This Game made my PC blow up...</p>
                     <p className='views-date'>350k views <RxDotFilled/> 4 days ago</p>
                 </div>
-                <div>
-                    <div>V</div>
+                <div className="channel-details">
+                    <div className="channel-icon">
+                        <p>V</p>
+                    </div>
                     <p className='channel-name'>Vienna</p>
                 </div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+                <p className="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
             </div>
         </div>
     )
