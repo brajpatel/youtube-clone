3ff5be1a93e07634dead94b8b967bc871276e9d1
brajpatel <brajeshpatel0002@gmail.com>
2023-06-13 11:26:51 +0100

    Style filter element

--- src/components/searched-results-page/SearchedResultsPage.js
+++ src/components/searched-results-page/SearchedResultsPage.js
@@ -1,9 +1,13 @@
 import "./SearchedResultsPage.css";
+import { BsFilterLeft } from "react-icons/bs";
 import SearchedVideoCard from "../video-cards/searched-video-card/SearchedVideoCard";
 
 function SearchedResultsPage() {
     return (
         <div className="searched-results-page">
+            <div className="filter">
+                <p><BsFilterLeft className="icon"/> Filters</p>
+            </div>
             <SearchedVideoCard/>
             <SearchedVideoCard/>
             <SearchedVideoCard/>
