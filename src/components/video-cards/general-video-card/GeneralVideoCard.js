c80d9a19d1db66dcf64357ad88e6c1946ac569b0
brajpatel <brajeshpatel0002@gmail.com>
2023-06-13 17:29:06 +0100

    Create function to format video view count

--- src/components/video-cards/general-video-card/GeneralVideoCard.js
+++ src/components/video-cards/general-video-card/GeneralVideoCard.js
@@ -1,11 +1,13 @@
 import './GeneralVideoCard.css';
 import { formatVideoDuration } from '../../../utils/formatVideoDuration';
+import { formatViewCount } from '../../../utils/formatViewCount';
 import { RxDotFilled } from "react-icons/rx";
 
 function GeneralVideoCard(props) {
     const { info } = props;
 
     let videoDuration = formatVideoDuration(info.contentDetails.duration);
+    let videoViews = formatViewCount(Number(info.statistics.viewCount));
 
     return (
         <div className="general-video-card">
@@ -21,7 +23,7 @@ function GeneralVideoCard(props) {
                 <div className='info'>
                     <p className='video-title'>{info.snippet.title}</p>
                     <p className='channel-name'>{info.snippet.channelTitle}</p>
-                    <p className='views-date'>350k views <RxDotFilled/> 4 days ago</p>
+                    <p className='views-date'>{videoViews} views <RxDotFilled/> 4 days ago</p>
                 </div>
             </div>
         </div>
---
+++ src/utils/formatViewCount.js
@@ -0,0 +1,13 @@
+export function formatViewCount(views) {
+    if (views >= 1000000000) {
+        return (views / 1000000000).toFixed(1).replace(/\.0$/, "") + "B";
+    }
+    if (views >= 1000000) {
+        return (views / 1000000).toFixed(1).replace(/\.0$/, "") + "M";
+    }
+    if (views >= 1000) {
+        return (views / 1000).toFixed(1).replace(/\.0$/, "") + "K";
+    }
+
+    return views;
+}
