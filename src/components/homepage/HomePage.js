e30122490e70165e1f971d83e825dea1b37e430a
brajpatel <brajeshpatel0002@gmail.com>
2023-06-13 15:38:25 +0100

    Adjust homepage gap

--- src/components/homepage/HomePage.js
+++ src/components/homepage/HomePage.js
@@ -1,7 +1,10 @@
 import "./HomePage.css";
+import { useState } from "react";
 import GeneralVideoCard from "../video-cards/general-video-card/GeneralVideoCard";
 
 function HomePage() {
+    const [videos, setVideos] = useState([]);
+    
     return (
         <div className="home-page">
             <GeneralVideoCard/>
