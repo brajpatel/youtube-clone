1ceb9ec00229c301fddad256a2fad2d43da4e0db
brajpatel <brajeshpatel0002@gmail.com>
2023-06-13 16:44:23 +0100

    Create get video duration function

---
+++ src/utils/getVideoDuration.js
@@ -0,0 +1,12 @@
+export function getVideoDuration(duration) {
+    let total = 0;
+    let hours = duration.match(/(\d+)H/);
+    let minutes = duration.match(/(\d+)M/);
+    let seconds = duration.match(/(\d+)S/);
+    
+    if (hours) total += parseInt(hours[1]) * 3600;
+    if (minutes) total += parseInt(minutes[1]) * 60;
+    if (seconds) total += parseInt(seconds[1]);
+
+    return total;
+}
