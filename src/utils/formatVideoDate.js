0ff598ef36e928ef8ceefd83c0a8300716ed9b9d
brajpatel <brajeshpatel0002@gmail.com>
2023-06-14 17:53:20 +0100

    Format date to display seconds minutes and hours

--- src/utils/formatVideoDate.js
+++ src/utils/formatVideoDate.js
@@ -1,5 +1,25 @@
 export function formatVideoDate(date) {
-    let current = new Date();
+    let videoDate = newDate(date);
+    let currentDate = new Date();
 
-    console.log(current);
+    let numDays = Math.floor((currentDate - videoDate) / 86400000);
+
+    if(!numDays) {
+        let numSeconds = Math.floor((currentDate - videoDate) / 1000);
+
+        if(numSeconds < 60) {
+            return numSeconds === 1 ? '1 second ago' : `${numSeconds} seconds ago`;
+        }
+        else if(numSeconds >= 60 && numSeconds < 3600) {
+            let numMinutes = Math.floor(numSeconds / 60);
+
+            return numMinutes === 1 ? '1 minute ago' : `${numMinutes} minutes ago`;
+        }
+        else {
+            let numMonths = Math.floor(numSeconds / 3600);
+
+            return numMonths === 1 ? '1 hours ago' : `${numMonths} hours ago`;
+        }
+    }
+    
 }
