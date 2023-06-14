cb5975f1e42ee5a318c3b91a214ea4f2693b07e5
brajpatel <brajeshpatel0002@gmail.com>
2023-06-14 18:01:16 +0100

    Rewrite view count function to be good

--- src/utils/formatViewCount.js
+++ src/utils/formatViewCount.js
@@ -1,13 +1,41 @@
 export function formatViewCount(views) {
-    if (views >= 1000000000) {
-        return (views / 1000000000).toFixed(1).replace(/\.0$/, "") + "B";
-    }
-    if (views >= 1000000) {
-        return (views / 1000000).toFixed(1).replace(/\.0$/, "") + "M";
-    }
-    if (views >= 1000) {
-        return (views / 1000).toFixed(1).replace(/\.0$/, "") + "K";
-    }
+    let viewsNum = Number(views);
+    let splitNum = views.split('');
 
-    return views;
+    if(viewsNum < 1000) {
+        return viewsNum === 1 ? '1 view' : `${views} views`;
+    }
+    else if (viewsNum >= 1000 && viewsNum < 1000000) {
+        let num;
+
+        if(splitNum.length === 4) {
+            num = splitNum[0];
+        }
+        else if(splitNum.length === 5) {
+            num = `${splitNum[0]}${splitNum[1]}`;
+        }
+        else {
+            num =`${splitNum[0]}${splitNum[1]}${splitNum[2]}`
+        }
+
+        return num + 'K views';
+    }
+    else if (viewsNum >= 1000000 && viewsNum < 1000000000) {
+        let num;
+
+        if(splitNum.length === 7) {
+            num = splitNum[0];
+        }
+        else if(splitNum.length === 8) {
+            num = `${splitNum[0]}${splitNum[1]}`;
+        }
+        else {
+            num =`${splitNum[0]}${splitNum[1]}${splitNum[2]}`
+        }
+
+        return num + 'M views';
+    }
+    else {
+        return splitNum.length === 10 ? `${splitNum[0]}B views` : `${splitNum[0]}${splitNum[1]}B views`;
+    }
 }
