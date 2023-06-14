490319abd06f9ae3f8b123bea3b427e5154d5c30
brajpatel <brajeshpatel0002@gmail.com>
2023-06-14 21:10:26 +0100

    Display data passed to each card

--- src/components/video-cards/searched-video-card/SearchedVideoCard.js
+++ src/components/video-cards/searched-video-card/SearchedVideoCard.js
@@ -28,22 +28,22 @@ function SearchedVideoCard(props) {
     return (
         <div className="searched-video-card">
             <div className="thumbnail">
-                <p>Image</p>
-                <p className="duration">11:29</p>
+                <img src={info.snippet.thumbnails.medium.url} alt='video-thumbnail'/>
+                <p className="duration">{videoDuration}</p>
             </div>
 
             <div className="info">
                 <div className="video-details">
-                    <p className="video-title">This Game made my PC blow up...</p>
-                    <p className='views-date'>350k views <RxDotFilled/> 4 days ago</p>
+                    <p className="video-title">{info.snippet.title}</p>
+                    <p className='views-date'>{videoViews} <RxDotFilled/> {videoDate}</p>
                 </div>
                 <div className="channel-details">
                     <div className="channel-icon">
-                        <p>V</p>
+                        <img src={channelIcon} alt="channel-icon"/>
                     </div>
-                    <p className='channel-name'>Vienna</p>
+                    <p>{info.snippet.channelTitle}</p>
                 </div>
-                <p className="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam sit amet, consectetur adipiscing. Sajkit amet, consectetur adipiscing.</p>
+                <p className="description">{videoDescription}</p>
             </div>
 
             <p className="video-actions"><HiOutlineDotsVertical/></p>
