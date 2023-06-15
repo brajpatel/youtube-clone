4f0fb71df6668b053514bc5479c15087589e27e1
brajpatel <brajeshpatel0002@gmail.com>
2023-06-15 20:57:53 +0100

    Remove use navigate

--- src/components/search-bar/SearchBar.js
+++ src/components/search-bar/SearchBar.js
@@ -1,12 +1,11 @@
 import "./SearchBar.css";
 import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { RxMagnifyingGlass } from "react-icons/rx";
 import { RxCross1 } from "react-icons/rx";
 
 function SearchBar(props) {
     const { setActiveTab } = props;
-    const navigate = useNavigate();
     
     const [input, setInput] = useState('');
 
