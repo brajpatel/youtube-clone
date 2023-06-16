4c12ca7708ae50cea38f7d014425ac2e93e1caca
brajpatel <brajeshpatel0002@gmail.com>
2023-06-16 20:58:19 +0100

    Add class names to preloader

--- src/components/preloader/Preloader.js
+++ src/components/preloader/Preloader.js
@@ -2,12 +2,14 @@ import './Preloader.css';
 
 function Preloader() {
     return (
-        <div>
-            <div className='dot' style={{"--delay": 1}}></div>
-            <div className='dot' style={{"--delay": 2}}></div>
-            <div className='dot' style={{"--delay": 3}}></div>
-            <div className='dot' style={{"--delay": 4}}></div>
-            <div className='dot' style={{"--delay": 5}}></div>
+        <div className='preloader-container'>
+            <div className='preloader'>
+                <div className='dot' style={{"--delay": 1}}></div>
+                <div className='dot' style={{"--delay": 2}}></div>
+                <div className='dot' style={{"--delay": 3}}></div>
+                <div className='dot' style={{"--delay": 4}}></div>
+                <div className='dot' style={{"--delay": 5}}></div>
+            </div>
         </div>
     )
 }
