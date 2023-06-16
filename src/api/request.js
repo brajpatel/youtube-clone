aa41e85cbdb8fc0315b1f915724ab200bb205958
brajpatel <brajeshpatel0002@gmail.com>
2023-06-16 21:23:09 +0100

    Definitely didn't change the api key

--- src/api/request.js
+++ src/api/request.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export const request = axios.create({
     baseURL: 'https://youtube.googleapis.com/youtube/v3',
     params: {
-        key:'AIzaSyB68oXZdXSwd-x5Eiw8320Y3HYC2iib_-0'
+        key:'AIzaSyCpxWdrUCQq-Th2WI5laYWzo30wpXWpt7k'
     }
 })
