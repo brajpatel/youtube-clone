5027388793ea2dcce042882a4d001fcb35291e8e
brajpatel <brajeshpatel0002@gmail.com>
2023-06-19 20:34:02 +0100

    Create watch page structure

--- src/components/watch-page/WatchPage.js
+++ src/components/watch-page/WatchPage.js
@@ -1,7 +1,43 @@
+import './WatchPage.css';
+import { useLocation } from 'react-router-dom';
+import { getChannelIcon } from "../../api/getChannelIcon";
+import { getChannelSubs } from "../../api/getChannelSubs";
+import { formatViewCount } from "../../utils/formatViewCount";
+import { useEffect, useState } from 'react';
+import {  } from 'react';
+
 function WatchPage() {
+    const location = useLocation();
+    const { state } = location;
+    const [channelIcon, setChannelIcon] = useState('');
+    const [channelSubs, setChannelSubs] = useState('');
+
+    useEffect(() => {
+        getChannelInfo();
+    }, [])
+
+    const getChannelInfo = async () => {
+        setChannelIcon(await getChannelIcon(state.snippet.channelId));
+        setChannelSubs(await getChannelIcon(state.snippet.channelId));
+    }
+
     return (
         <div>
-            Watch Page
+            
+            <div>
+                <div>
+                    <div>
+                        video and channel stuff
+                    </div>
+                    <div>
+                        comments
+                    </div>
+                </div>
+
+                <div>
+                    related videos
+                </div>
+            </div>
         </div>
     )
 }
