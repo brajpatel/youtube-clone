ed6c84d044497c6f17849634615f57b2386d2417
brajpatel <brajeshpatel0002@gmail.com>
2023-06-20 20:23:27 +0100

    Create video details component

---
+++ src/components/watch-page/video-details/VideoDetails.js
@@ -0,0 +1,19 @@
+import './VideoDetailscss';
+import { formatViewCount } from '../../../utils/formatViewCount';
+import { FiThumbsUp } from "react-icons/fi";
+import { FiThumbsDown } from "react-icons/fi";
+import { RiShareForwardLine } from "react-icons/ri";
+import { BsThreeDots } from "react-icons/bs";
+
+function VideoDetails(props) {
+    const { state, channelIcon, channelSubs } = props;
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+
+export default VideoDetails;
