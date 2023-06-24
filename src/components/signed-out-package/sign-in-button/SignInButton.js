58ee5d1c40f738ffb3c210af1fe94a46567cba5a
brajpatel <brajeshpatel0002@gmail.com>
2023-06-24 12:12:02 +0100

    Add sign in onclick to sign in button

--- src/components/signed-out-package/sign-in-button/SignInButton.js
+++ src/components/signed-out-package/sign-in-button/SignInButton.js
@@ -2,8 +2,10 @@ import "./SignInButton.css";
 import { FaRegUserCircle } from "react-icons/fa";
 
 function SignInButton() {
+    const { signIn } = useContext(AuthContext);
+
     return (
-        <button className="sign-in-button"><FaRegUserCircle/>Sign in</button>
+        <button className="sign-in-button" onClick={signIn}><FaRegUserCircle/>Sign in</button>
     )
 }
 
